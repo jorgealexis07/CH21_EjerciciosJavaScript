86adfe294b80fb23207ae126ae843b5382a52b86
jorgealexis07 <201711104@tese.edu.mx>
2022-12-10 16:10:33 -0600

    Agrego el ejercicio #2

--- js/main.js
+++ js/main.js
@@ -10,3 +10,16 @@ function NumeroMayor(Numeros){
 }
 let Numeros=[3, 5, 7, 1, 6];
 console.log(NumeroMayor(Numeros)); 
+
+//Ejercicio #2
+function NumeroMenor(Numeros){
+    let menor=0;
+    for (let i = 0; i < Numeros.length; i++) {
+        if (Numeros[i] <  menor) {
+            menor = Numeros[i];
+        }
+    }
+    return menor;
+}
+Numeros=[-1, 3, 4, 2, 6];
+console.log(NumeroMenor(Numeros)); 
