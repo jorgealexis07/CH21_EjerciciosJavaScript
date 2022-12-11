38caa11e480d8751207e8327a38e5433308a7672
jorgealexis07 <201711104@tese.edu.mx>
2022-12-10 22:43:53 -0600

    Agrego ejercicios 1 y 2 parte 2

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ function NumeroMayor(Numeros) {
   }
   return mayor;
 }
+
 let Numeros = [3, 5, 7, 1, 6];
 console.log(NumeroMayor(Numeros));
 
@@ -45,3 +46,29 @@ const ArrayDesordenado = ar => ar.sort(() => Math.random()-0.5);
 Numeros = [1, 2, 5, 14, 24, 31, 50, 105];
 console.log("Numeros Ordenados: "+Numeros);
 console.log("Numeros Desordenados: "+ArrayDesordenado(Numeros));
+
+
+//Segunda Parte 
+let btnCalcularEJ1 = document.getElementById("btnCalcularEJ1");
+btnCalcularEJ1.addEventListener("click", function (event){
+    event.preventDefault();
+    let num1 = parseInt(document.getElementById("inputNum1").value);
+    let num2 = parseInt(document.getElementById("inputNum2").value);
+    let num3 = parseInt(document.getElementById("inputNum3").value);
+    let Result = document.getElementById("displayej1");
+    
+    let Numeros = [num1,num2,num3];
+    Result.innerHTML= NumeroMayor(Numeros);
+});
+
+let btnCalcularEJ2 = document.getElementById("btnCalcularEJ2");
+btnCalcularEJ2.addEventListener("click", function (event) {
+    event.preventDefault();
+    let num1 = parseInt(document.getElementById("inputNum1").value);
+    let num2 = parseInt(document.getElementById("inputNum2").value);
+    let num3 = parseInt(document.getElementById("inputNum3").value);
+    let Result = document.getElementById("displayej1");
+    
+    let Numeros = [num1,num2,num3];
+    Result.innerHTML = NumeroMenor(Numeros);
+} );
